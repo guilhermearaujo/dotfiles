f7eeac587a087a604cc5d7eb57dd6dae9c87976b
Guilherme Araújo <guilhermeama@gmail.com>
2021-05-19 20:43:30 -0300

    Update brew install command

--- setup-macOS.sh
+++ setup-macOS.sh
@@ -54,7 +54,8 @@ echo "| Installing Homebrew...      |"
 echo "+-----------------------------+"
 echo
 
-yes | /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+eval "$(/opt/homebrew/bin/brew shellenv)"
 
 brew tap heroku/brew &> /dev/null
 brew tap homebrew/cask-fonts &> /dev/null
